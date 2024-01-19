4e3c2e02b9797ef1f761283aa7712fd8790c78ba
Horaclee <115532821+Horaclee@users.noreply.github.com>
2024-01-19 14:22:25 +0100

    Update Solution1.cpp

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,40 +1,41 @@
 #include "Solution1.h"
-#include <iomanip>
-// Don't fortget to enable the exercise in the SudentConfiguration.h file !
+
+// N'oubliez pas d'activer l'exercice dans le fichier StudentConfiguration.h !
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_1
 
-float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
-{
-    if (_from == TemperatureUnits::KELVIN || _to == TemperatureUnits::CELSIUS)
-    {
-        return std::round((_value - 273) * 100) / 100; 
-    }
-    if (_from == TemperatureUnits::KELVIN || _to == TemperatureUnits::FAHRENHEIT)
-    {
-        return std::round((9 / 5 * (_value - 273) + 32) * 100) / 100;
-    }
-    if (_from == TemperatureUnits::FAHRENHEIT || _to == TemperatureUnits::KELVIN)
-    {
-        return std::round((5 / 9 * (_value - 32) + 273) * 100) / 100;
-    }
-    if (_from == TemperatureUnits::FAHRENHEIT || _to == TemperatureUnits::CELSIUS)
-    {
-        return std::round((5 / 9 * (_value - 32)) * 100) / 100;
-    }
-    if (_from == TemperatureUnits::CELSIUS || _to == TemperatureUnits::KELVIN)
-    {
-        return std::round((_value + 273) * 100) / 100;
-    }
-    if (_from == TemperatureUnits::CELSIUS || _to == TemperatureUnits::FAHRENHEIT)
-    {
-        return std::round((9 / 5 * (_value)+32) * 100) / 100;
-    }
-    if (_from == _to)
-    {
-        return std::round(_value * 100) / 100;
+float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to) {
+    if (_from == _to) {
+        return _value; // No conversion needed for the same units
     }
 
+    switch (_from) {
+    case TemperatureUnits::CELSIUS:
+        if (_to == TemperatureUnits::FAHRENHEIT) {
+            return 9.0 / 5.0 * _value + 32.0;
+        }
+        else if (_to == TemperatureUnits::KELVIN) {
+            return _value + 273.15;
+        }
+        break;
+
+    case TemperatureUnits::FAHRENHEIT:
+        if (_to == TemperatureUnits::CELSIUS) {
+            return 5.0 / 9.0 * (_value - 32.0);
+        }
+        else if (_to == TemperatureUnits::KELVIN) {
+            return 5.0 / 9.0 * (_value - 32.0) + 273.15;
+        }
+        break;
+
+    case TemperatureUnits::KELVIN:
+        if (_to == TemperatureUnits::CELSIUS) {
+            return _value - 273.15;
+        }
+        else if (_to == TemperatureUnits::FAHRENHEIT) {
+            return 9.0 / 5.0 * (_value - 273.15) + 32.0;
+        }
+        break;
+    }
 }
-
 #endif
