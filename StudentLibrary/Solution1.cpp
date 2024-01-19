09f0bb9756ef9ebb5d529de6bc4eafdbccb9a78a
Horaclee <115532821+Horaclee@users.noreply.github.com>
2024-01-19 14:07:58 +0100

    Update Solution1.cpp

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,39 +1,39 @@
 #include "Solution1.h"
-
+#include <iomanip>
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_1
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	if (_from == TemperatureUnits::KELVIN || _to == TemperatureUnits::CELSIUS)
-	{
-		return _value - 273;
-	}
-	if (_from == TemperatureUnits::KELVIN || _to == TemperatureUnits::FAHRENHEIT)
-	{
-		return 9 / 5 * (_value - 273) + 32;
-	}
-	if (_from == TemperatureUnits::FAHRENHEIT || _to == TemperatureUnits::KELVIN)
-	{
-		return 5 / 9 * (_value - 32) + 273;
-	}
-	if (_from == TemperatureUnits::FAHRENHEIT || _to == TemperatureUnits::CELSIUS)
-	{
-		return 5 / 9 * (_value - 32);
-	}
-	if (_from == TemperatureUnits::CELSIUS || _to == TemperatureUnits::KELVIN)
-	{
-		return _value + 273;
-	}
-	if (_from == TemperatureUnits::CELSIUS || _to == TemperatureUnits::FAHRENHEIT)
-	{
-		return 9 / 5 * (_value) + 32;
-	}
-	if (_from == _to)
-	{
-		return _value;
-	}
+    if (_from == TemperatureUnits::KELVIN || _to == TemperatureUnits::CELSIUS)
+    {
+        return std::round((_value - 273) * 100) / 100; 
+    }
+    if (_from == TemperatureUnits::KELVIN || _to == TemperatureUnits::FAHRENHEIT)
+    {
+        return std::round((9 / 5 * (_value - 273) + 32) * 100) / 100;
+    }
+    if (_from == TemperatureUnits::FAHRENHEIT || _to == TemperatureUnits::KELVIN)
+    {
+        return std::round((5 / 9 * (_value - 32) + 273) * 100) / 100;
+    }
+    if (_from == TemperatureUnits::FAHRENHEIT || _to == TemperatureUnits::CELSIUS)
+    {
+        return std::round((5 / 9 * (_value - 32)) * 100) / 100;
+    }
+    if (_from == TemperatureUnits::CELSIUS || _to == TemperatureUnits::KELVIN)
+    {
+        return std::round((_value + 273) * 100) / 100;
+    }
+    if (_from == TemperatureUnits::CELSIUS || _to == TemperatureUnits::FAHRENHEIT)
+    {
+        return std::round((9 / 5 * (_value)+32) * 100) / 100;
+    }
+    if (_from == _to)
+    {
+        return std::round(_value * 100) / 100;
+    }
 
 }
 
