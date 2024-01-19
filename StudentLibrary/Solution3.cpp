caa49845652dde8dc0091c5ebe40d24abebe3577
Horaclee <115532821+Horaclee@users.noreply.github.com>
2024-01-19 14:48:26 +0100

    Update Solution3.cpp

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,5 +1,8 @@
 #include "Solution3.h"
-
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <cctype> 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_3
@@ -9,8 +12,24 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 	words = _words;
 }
 
-void Solution3::SortWords() {}
+void Solution3::SortWords() {
+    if (words.empty()) {
+        throw std::logic_error("The vector is null or empty.");
+    }
 
+    // Convert each word to lowercase before sorting
+    std::transform(words.begin(), words.end(), words.begin(),
+        [](std::string& word) {
+            std::transform(word.begin(), word.end(), word.begin(), [](unsigned char c) {
+                return std::tolower(c);
+                });
+            return word;
+        });
+
+    // Sort the words
+    std::sort(words.begin(), words.end());
+}
+ 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
 	return words;
